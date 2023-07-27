54649a61999bb620be76a1b455382d3d67f57ac4
ArunaKReddy <112746101+ArunaKReddy@users.noreply.github.com>
2023-07-27 06:30:53 +0000

    variables

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,5 @@
 console.log("Aruna ")
+const AccountHolderNAme="Aruna"
+let AccountHolderNumber=1234567
+let AccountHolderAddress
+console.table([AccountHolderNAme,AccountHolderNumber,AccountHolderAddress])
